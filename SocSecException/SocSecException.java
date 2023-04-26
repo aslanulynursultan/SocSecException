5c85025663ef191355082a6368e4c003b2482df8
Nursultan Aslanuly <95742399+aslanulynursultan@users.noreply.github.com>
2023-04-26 19:15:30 +0200

    Add files via upload

---
+++ SocSecException/SocSecException.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+import java.lang.Exception;
+public class SocSecException extends Exception
+{
+    private String error;
+
+    public SocSecException(String error)
+    {
+        super(error);
+        this.error = error;
+    }
+    public String toString() {
+        return "Invalid Social Security Number, " +error;
+    }
+}
+
+
+class SocSecProcessor{
+
+    static SocSecException excep;
+    static String error="";
+
+    public static void main(String[] args)
+    {
+        String name,SSN="";
+        String choice="";
+
+        boolean terminate = false;
+
+        while(!terminate)
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.printf("Enter Name\t: ");
+            name = input.nextLine();
+            System.out.printf("Enter SSN\t: ");
+            SSN = input.next();
+
+            if(isValid(SSN))
+            {
+                System.out.println(name+" "+SSN+" is valid");
+            }
+            else
+                System.out.println(error);
+
+            while(true)
+            {
+                System.out.printf("Continue(y/n)\t: ");
+                choice = input.next();
+                choice = choice.toLowerCase();
+
+                if(choice.equals("n"))
+                {
+                    terminate = true;
+                    break;
+                }
+                else if(choice.equals("y"))
+                {
+                    break;
+                }
+            }
+        }
+    }
+    public static boolean isValid(String ssn)
+    {
+        boolean valid = true;
+
+        try{
+            if(ssn.length() != 11)
+            {
+                throw new SocSecException("wrong number of characters");
+            }
+            else if(ssn.charAt(3) != '-' || ssn.charAt(6) != '-')
+            {
+                throw new SocSecException("dashes at wrong position");
+            }
+            else
+            {
+                for(int x=0;x<ssn.length();x++)
+                {
+                    if(ssn.charAt(x) == '-' || Character.isDigit(ssn.charAt(x)))
+                        continue;
+                    else
+                    {
+                        throw new SocSecException("contains a character that is not a digit");
+                    }
+                }
+            }
+        }catch(SocSecException e)
+        {
+            error = e.toString();
+            valid = false;
+        }
+
+        return valid;
+    }
+
+}
